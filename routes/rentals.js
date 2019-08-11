94d39e0b645ca48ef885d20ee4295d6011aa4a3c
akikgt <komoria@oregonstate.edu>
2019-08-11 21:44:55 +0900

    Add rentals router

---
+++ routes/rentals.js
@@ -0,0 +1,87 @@
+const {Rental, validate} = require('../models/rental');
+const {Movie} = require('../models/movie');
+const {Customer} = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.send(rentals);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send('Invalid customer.');
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send('Invalid movie.');
+
+  if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock');
+
+  let rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      isGold: customer.isGold,
+      phone: customer.phone
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate
+    }
+  });
+  rental = await rental.save();
+
+  movie.numberInStock--;
+  movie.save();
+
+  res.send(rental);
+});
+
+// router.put('/:id', async (req, res) => {
+//   const { error } = validate(req.body); 
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   const genre = await Genre.findById(req.body.genreId);
+//   if (!genre) return res.status(400).send('Invalid genre.');
+
+//   const rental = await Rental.findByIdAndUpdate(req.params.id,
+//     { title: req.body.title,
+//       genre: {
+//         _id: genre._id,
+//         name: genre.name
+//       },
+//       numberInStock: req.body.numberInStock,
+//       dailyRentalRate: req.body.dailyRentalRate
+//      },
+//     {
+//       new: true
+//     });
+
+//   if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+  
+//   res.send(rental);
+// });
+
+// router.delete('/:id', async (req, res) => {
+//   const rental = await Rental.findByIdAndRemove(req.params.id);
+
+//   if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+
+//   res.send(rental);
+// });
+
+router.get('/:id', async (req, res) => {
+  const rental = await Rental.findById(req.params.id);
+
+  if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+
+  res.send(rental);
+});
+
+module.exports = router;
