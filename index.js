ae154234b86a963f0c48c4d7e33b2c07957f874c
akikgt <komoria@oregonstate.edu>
2019-08-11 23:11:41 +0900

    Add usres router

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly')
+mongoose.connect('mongodb://localhost/vidly', {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB.'));
 
@@ -17,6 +21,7 @@ app.use('/api/genres/', genres);
 app.use('/api/customers/', customers);
 app.use('/api/movies/', movies);
 app.use('/api/rentals/', rentals);
+app.use('/api/users/', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
