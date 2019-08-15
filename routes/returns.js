b179eb49a0dca35edf2c9bfdafae1299b0e0f3f9
akikgt <komoria@oregonstate.edu>
2019-08-16 02:03:13 +0900

    Add tests for return

--- routes/returns.js
+++ routes/returns.js
@@ -11,6 +11,13 @@ router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    let rental = Rental.find({
+        customerId: req.body.customerId,
+        movieId: req.body.movieId
+    });
+
+    if (!rental) return res.status(404).send('No rental found');
+
     res.send(200);
 });
 
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const {User} = require('../../models/user');
 const {Rental} = require('../../models/rental');
 const mongoose = require('mongoose');
 
@@ -8,6 +9,7 @@ describe('/api/returns', () => {
     let customerId;
     let movieId;
     let rental;
+    let token;
 
     beforeEach(async () => {
         server = require('../../index');
@@ -31,15 +33,47 @@ describe('/api/returns', () => {
         await rental.save();
     });
     afterEach(async () => {
-        server.close();
+        await server.close();
         await Rental.remove({});
     });
 
-    it('should return 401 if client is not logged in', async () => {
-        const res = await request(server)
+
+    beforeEach(() => {
+        token = new User().generateAuthToken();
+    });
+
+    const exec = async () => {
+        return await request(server)
             .post('/api/returns')
-            .send({ customerId: customerId, movieId: movieId });
+            .set('x-auth-token', token)
+            .send({ customerId , movieId });
+    };
+
+    it('should return 401 if client is not logged in', async () => {
+        token = '';
+        const res = await exec();
 
         expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if customerId is not provided', async () => {
+        customerId = null;
+        const res =  await exec();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 400 if movieId is not provided', async () => {
+        movieId = null;
+        const res =  await exec();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 404 if no rental found for this customer/movie', async () => {
+        movieId = 1;
+        const res =  await exec();
+
+        expect(res.status).toBe(400);
     });
 });
