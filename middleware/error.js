b3af8dbb2c10b9755e5f0d79ed95a2c895d6c9f7
akikgt <komoria@oregonstate.edu>
2019-08-15 14:00:22 +0900

    Add winston

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,13 @@
+const winston = require('winston');
+
 
 module.exports = function(err, req, res, next) {
+    // winston.error(err.message, [{metaKey: err}]);
+    winston.log({
+        level: 'error',
+        message: err.message,
+        meta: err
+    });
+
     res.status(500).send("Something failed.");
 }
