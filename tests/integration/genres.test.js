d3b04f83f5db7b894d0767f3a19779ce09302ef3
akikgt <komoria@oregonstate.edu>
2019-08-15 17:45:56 +0900

    Add unauthentication test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -35,5 +35,20 @@ describe('/api/genres', () => {
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty('name', genre.name);
         });
+
+        it('should return 404 if invalid id is passed', async () => {
+            const res = await request(server).get('/api/genres/1');
+            expect(res.status).toBe(404);
+        });
     });
+
+    describe('POST /', () => {
+        it('should return 401 if client is not logged in', async () => {
+            const res = await request(server)
+                .post('/api/genres')
+                .send({ name: 'genre1' });
+
+            expect(res.status).toBe(401);
+        })
+    })
 });
